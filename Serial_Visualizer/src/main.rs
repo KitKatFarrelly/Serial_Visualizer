2fb9408c01b1f48e165ed6ceeff8f4ca9e51a5a6
KitKatFarrelly <49258630+KitKatFarrelly@users.noreply.github.com>
2024-05-14 01:48:03 -0400

    added basic console log

--- Serial_Visualizer/src/main.rs
+++ Serial_Visualizer/src/main.rs
@@ -1,9 +1,11 @@
 use eframe::egui;
+use egui::{RichText, FontId, Color32};
 
-fn main() -> Result<(), eframe::Error>  {
+fn main() -> Result<(), eframe::Error>  
+{
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([720.0, 480.0]),
         ..Default::default()
     };
     eframe::run_native(
@@ -16,35 +18,51 @@ fn main() -> Result<(), eframe::Error>  {
 struct MainFrame
 {
     //Put State here
-    name: String,
-    age: u32,
+    connect_button_color: Color32,
+    num_rows: u32,
 }
 
-impl Default for MainFrame {
-    fn default() -> Self {
-        Self {
+impl Default for MainFrame 
+{
+    fn default() -> Self 
+    {
+        Self 
+        {
             //Put defaults for data in the MainFrame Struct
-            name: "Arthur".to_owned(),
-            age: 42,
+            connect_button_color: Color32::RED,
+            num_rows: 30,
         }
     }
 }
 
 
-impl eframe::App for MainFrame {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
+impl eframe::App for MainFrame 
+{
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) 
+    {
+        egui::CentralPanel::default().show(ctx, |ui|
+        {
+            if ui.add(egui::Button::new(RichText::new(format!("Connect To Robot")).color(Color32::BLACK).font(FontId::proportional(20.0))).fill(self.connect_button_color)).clicked()
+            {
+                //send some serial data through COM Port. Change Color once connected.
+                self.connect_button_color = Color32::GREEN;
             }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            //Console Logs at bottom
+            ui.heading("Output Log:");
+            let default_spacing = ui.spacing().item_spacing.y;
+            ui.spacing_mut().item_spacing.y = 0.0;
+            egui::ScrollArea::vertical().auto_shrink([false; 2]).max_height(400.0).max_width(1130.0).show(ui, |ui|
+            {
+                for row in 0..self.num_rows 
+                {
+                    ui.add(egui::Label::new(RichText::new(format!("{:160}",
+                        "placeholder text for console log. Replace with actual console log text."))
+                        .color(Color32::GREEN)
+                        .background_color(Color32::BLACK)
+                        .font(FontId::monospace(12.0))).truncate(true));
+                }
+            });
+            
         });
     }
 }
